e153e66be82269efcfd787e4cf55b4aee5472390
xexxw <90753521+xexxw@users.noreply.github.com>
2023-11-23 20:16:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///microblog.db'
 db = SQLAlchemy(app)
 
-# 导入其他模块
-from models import User, Tweet, Follow, Like
-from routes import post_content_api, follow_api, unfollow_api, get_feed_api, like_api, unlike_api, get_hot_contents_api
-
-# 创建数据库表
-db.create_all()
-
 if __name__ == '__main__':
+    with app.app_context():
+        from routes import post_content_api, follow_api, unfollow_api, get_feed_api, like_api, unlike_api, get_hot_contents_api
+        db.create_all()
     app.run(debug=True)
